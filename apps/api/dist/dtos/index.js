f43ebd03a58319197eb113d9aa5bc237ccac6f1c
Thiago-DA <thiagodan@tuta.io>
2026-09-22 19:56:38 -0300

    misc: Quitar carpetas dist

--- apps/api/dist/dtos/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/**
- * Data Transfer Objects (DTOs) para RentAR
- * Cada tabla de la base de datos cuenta con su DTO representativo.
- */
-Object.defineProperty(exports, "__esModule", { value: true });
--- packages/shared-types/dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-/**
- * Shared Types for RentAR
- */
-Object.defineProperty(exports, "__esModule", { value: true });
